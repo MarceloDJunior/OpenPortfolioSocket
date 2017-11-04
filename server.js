90664549693051a49f843c6fc7d650e984eaa671
Marcelo Dornelles Junior <147869@upf.br>
2017-11-04 14:31:55 -0200

    express

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const io = require('socket.io')();
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-const port = 8000;
-io.listen(port);
-console.log('listening on port ', port);
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX) )
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
 
 io.on('connection', (client) => {
     console.log('user connected');
