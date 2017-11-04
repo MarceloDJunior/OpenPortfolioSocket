5b5a546eb11ac716c6dc7dfeb5f59a9984abffa8
Marcelo Dornelles Junior <147869@upf.br>
2017-11-04 16:56:28 -0200

    Heroku fix

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const server = express()
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server, /*{
-    pingInterval: 15000,
-    pingTimeout: 30000,
-}*/);
+const io = socketIO(server);
 
 io.on('connection', (client) => {
 
